4ca3122404d6c046a070f381ee4af7674a6e343d
dangrt5 <randy.dang05@gmail.com>
2018-09-18 00:16:37 -0700

    Form input prototype feature set 5 finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import ContactList from "./contact-list";
 import ContactForm from "./contact-form";
+import contactData from "./contacts"
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1 className="text-center my-3">Address Book</h1>
-      <div className="row">
-        <div className="col-4">
-          <ContactForm/>
+class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      contacts: contactData
+    }
+  }
+
+
+  render() {
+  return  (
+      <div className="container">
+        <h1 className="text-center my-3">Address Book</h1>
+        <div className="row">
+          <div className="col-4">
+            <ContactForm/>
+          </div>
+          <ContactList contacts={this.state.contacts}/>
         </div>
-        <ContactList/>
       </div>
-    </div>
-  )
+        )
+  }
 }
 
 export default App;
